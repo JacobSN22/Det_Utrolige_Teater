0be635f771f880f68fe62cdc24a0982f2c4123b8
Jacob <jacobsimoninielsen@gmail.com>
2023-09-05 13:14:20 +0200

    Home page og skuespiller
    Er nu blevet færdig med at lave home page og selve skuespiller siden, dog ikke læs mere functionerne

--- frontend/src/components/pages/home.jsx
+++ frontend/src/components/pages/home.jsx
@@ -13,7 +13,6 @@ export const Home = () => {
       const result = await axios.get(url)
       // console.log(result.data);
       setImages(result.data.slice(0, 1))
-      console.log(result.data);
     }
     getdata()
   }, []);
@@ -28,7 +27,47 @@ export const Home = () => {
             return (
               <>
                 <div className='container'>
+                  <div>
+                    <p>STOR SCENE</p>
+                    <p>28. APRIL - 30 APRIL 2023</p>
+                    
+                    <h2>Fyrtøjet</h2>
+                    <h3>BØRNEFORFATTER</h3>
+                  </div>
+                  
                   <img key={item.id} src={`http://localhost:4000/Assets/Images/events/medium/${item.image}`} alt="Billed" />
+                </div>
+
+                <div>
+                  <div>
+                    <img key={item.id} src={`http://localhost:4000/Assets/Images/events/medium/${item.image}`} alt="Billed" />
+                    <p>STOR SCENE</p>
+                    <p>2. JUNI - 6. JUNI 2023</p>
+                    <h2>Downtown<br />Revyen</h2>
+                    <h3>AF MICHEAL ENGLER</h3>
+                    <button>LÆS MERE</button>
+                    <button>KØB BILLET</button>
+                  </div>
+
+                  <div>
+                    <img key={item.id} src={`http://localhost:4000/Assets/Images/events/medium/${item.image}`} alt="Billed" />
+                    <p>FRISCENEN</p>
+                    <p>13. MAJ - 17. MAJ 2023</p>
+                    <h2>Mit<br />Nordkraft</h2>
+                    <h3>MUSIKTEATER</h3>
+                    <button>LÆS MERE</button>
+                    <button>KØB BILLET</button>
+                  </div>
+
+                  <div>
+                    <img key={item.id} src={`http://localhost:4000/Assets/Images/events/medium/${item.image}`} alt="Billed" />
+                    <p>LILLE SCENE</p>
+                    <p>23. DECEMBER - 25. DECEMBER 2022</p>
+                    <h2>Det lever<br />i skyggerne</h2>
+                    <h3>AF ENMAS GARCIA</h3>
+                    <button>LÆS MERE</button>
+                    <button>KØB BILLET</button>
+                  </div>
                 </div>
               </>
             )
--- frontend/src/components/pages/skuespillere.jsx
+++ frontend/src/components/pages/skuespillere.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../partials/header'
 import { Footer } from '../partials/footer'
+import axios from 'axios';
 
 export const Skuespillere = () => {
+  
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const getdata = async () => {
+      const url = `http://localhost:4000/actors?attributes=id, name, image,description`;
+      const result = await axios.get(url)
+      // console.log(result.data);
+      setImages(result.data.slice(0, 2))
+    }
+    getdata()
+  }, []);
+
+
+
   return (
     <div>
-      <Header />
+      <Header />  
+      {images && images.map(item => {
+            return (
+              <>
+                <h1>Skuespillere</h1>
+                <div className='container'>
+                  <img key={item.id} src={`http://localhost:4000/Assets/Images/actors/${item.image}`} alt="Billed" />
+                  <h3>{item.name}</h3>
+                  <p>{item.description}</p>
+                  <button>LÆS MERE</button>
+                </div>
+              </>
+            )
 
+          })}
       <Footer />
     </div>
   )
